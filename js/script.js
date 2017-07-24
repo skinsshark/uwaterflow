2994124560947102a3d2359db24b3838f898a9d2
sharon zheng <sharon.zheng@live.com>
2017-07-24 10:07:24 -0700

    update course code regex

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
   //find all course titles as defined in the DOM
   const list_of_courses = document.querySelectorAll('table tbody tr td[align=left] b');
   //regex to group 1: course code, 2: other commitments to the course
-  const reg = /<a name=".*"><\/a>([A-Z]* \d* [A-Z]?)( .*)/;
+  const reg = /<a name=".*"><\/a>([A-Z]* \d*[A-Z]?)( .*)/;
 
   for (let i = 0; i < list_of_courses.length; i++) {
     $(list_of_courses[i]).addClass('outflow'); //add class for styling
